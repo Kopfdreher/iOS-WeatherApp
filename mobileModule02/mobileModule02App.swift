fa0b6a80d838ebe7f9846cc9919e76c09e1445d4
Sergej Gavrilov <sgavrilov@kopfdreher.de>
2026-04-15 19:31:05 +0200

    Initial Commit

---
+++ mobileModule02/mobileModule02App.swift
@@ -0,0 +1,17 @@
+//
+//  mobileModule02App.swift
+//  mobileModule02
+//
+//  Created by Sergej Gavrilov on 15.04.26.
+//
+
+import SwiftUI
+
+@main
+struct mobileModule02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
